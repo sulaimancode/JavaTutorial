8f786d395b420845a870e37f11d42c4d61fc5002
slaiman ahmad <sulaimanahmad321@gmail.com>
2017-10-01 03:30:34 +0100

    User Input + getters and setters

--- Java Tutorial/src/Animal.java
+++ Java Tutorial/src/Animal.java
@@ -42,8 +42,84 @@ public class Animal {
 		
 		int modOfNumbers = 5 % 3;
 		System.out.println("5 % 3 = " + modOfNumbers);
+		
+		System.out.print("Enter the name: \n");
+		
+		// hasNextInt, hasNextFloat, hasNextDouble, hasNextBoolean
+		// hasNextByte
+		// nextInt, nextFloat, nextDouble
+		
+		if(userInput.hasNextLine()) {
+			
+			this.setName(userInput.nextLine());
+			
+		}
 	}
 	
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getWeight() {
+		return weight;
+	}
+
+	public void setWeight(int weight) {
+		this.weight = weight;
+	}
+
+	public boolean isHasOwner() {
+		return hasOwner;
+	}
+
+	public void setHasOwner(boolean hasOwner) {
+		this.hasOwner = hasOwner;
+	}
+
+	public byte getAge() {
+		return age;
+	}
+
+	public void setAge(byte age) {
+		this.age = age;
+	}
+
+	public long getUniqueID() {
+		return uniqueID;
+	}
+
+	public void setUniqueID(long uniqueID) {
+		this.uniqueID = uniqueID;
+	}
+
+	public char getFavoriteChar() {
+		return favoriteChar;
+	}
+
+	public void setFavoriteChar(char favoriteChar) {
+		this.favoriteChar = favoriteChar;
+	}
+
+	public double getSpeed() {
+		return speed;
+	}
+
+	public void setSpeed(double speed) {
+		this.speed = speed;
+	}
+
+	public float getHeight() {
+		return height;
+	}
+
+	public void setHeight(float height) {
+		this.height = height;
+	}
+
 	public static void main(String[] args) {
 		
 		Animal theAnimal = new Animal();
